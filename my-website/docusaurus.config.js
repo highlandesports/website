dd81731835b2ce552cc265cfb137a70be8877490
David Hopwood <179640049+DaRkTodash@users.noreply.github.com>
2024-09-04 12:30:54 -0600

    Update docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -8,12 +8,12 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Highland Esports',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://highlandesports.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
